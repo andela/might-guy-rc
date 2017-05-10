d8664ddbb8fc470d470005a5102bdb268cbe164d
andela-moseni <mercy.oseni@andela.com>
2017-05-10 22:18:12 +0100

    feature(RESTful API): modify routes permission based on priviledges

--- server/startup/restful-endpoints.js
+++ server/startup/restful-endpoints.js
@@ -1,5 +1,11 @@
 import { Shops, Products, Orders, Cart, Accounts, Emails, Shipping }
 from "/lib/collections";
+import { Roles } from "meteor/alanning:roles";
+import Reaction  from "/server/api/core";
+
+const hasPermission = (user, role) => {
+  return user.roles[Reaction.getShopId()].includes(role)
+}
 
 export default () => {
   // Global API configuration
@@ -8,66 +14,33 @@ export default () => {
     prettyJson: true,
     defaultHeaders: {
       "Content-Type": "application/json"
-    }
+    },
   });
 
-  const getApiOptions = (collectionName, collectionString) => {
-    let isAuth;
-    if (collectionString === 'Products' || collectionString === 'Shops') {
-      isAuth = false;
-    } else { 
-      isAuth = true;
-    }
+  const getApiOptions = (collectionName) => {
     return {
       routeOptions: {
-        authRequired: isAuth
+        authRequired: true
       },
+      
       endpoints: {
-        // GET all items in collection
-        get: {
-          action() {
-            const allRecords = collectionName.find();
-            if (allRecords) {
-              return { statusCode: 201, status: "success", data: allRecords };
-            }
-            return {
-              statusCode: 404,
-              status: "fail",
-              message: "error"
-            };
-          }
-        },
-
-        // GET an item in collections by id
-        get: {
-          action() {
-            const oneRecord = collectionName.findOne(this.urlParams.id);
-            if (oneRecord) {
-              return { statusCode: 201, status: "success", data: oneRecord };
-            }
-            return {
-              statusCode: 404,
-              status: "fail",
-              message: "record does not exist"
-            };
-          }
-        },
-
         // POST into a collection
         post: {
           authRequired: true,
+          roleRequired: ["admin", "owner", "guest"],
           action() {
             const isInserted = collectionName.insert(this.bodyParams);
             if (isInserted) {
               return { statusCode: 201, status: "success", data: isInserted };
             }
-            return { status: "fail", message: "error" };
+            return { status: "fail", message: "post was not successful" };
           }
         },
 
         // UPDATE a collection
         put: {
           authRequired: true,
+          roleRequired: ["admin", "owner", "guest"],
           action() {
             const isUpdated = collectionName.update(this.urlParams.id, {
               $set: this.bodyParams
@@ -82,9 +55,9 @@ export default () => {
         // DELETE a record in a collection
         delete: {
           authRequired: true,
+          roleRequired: ["admin"],
           action() {
             const isDeleted = collectionName.remove(this.urlParams.id);
-
             if (isDeleted) {
               return { status: "success", data: { message: "record deleted" } };
             }
@@ -94,12 +67,30 @@ export default () => {
       }
     };
   };
+  
+  Api.addRoute('Emails', { authRequired: true}, {
+    get: function() {
+      const email = this.user.emails[0].address;
+      const query = hasPermission(this.user, "admin") ? {} :
+      { $or: [{to : email  }, {from : email }]}
+      const allRecords = Emails.find(query).fetch();
+        return { data: allRecords };
+    } 
+  });
 
-  Api.addCollection(Shops, getApiOptions(Shops, 'Shops'));
-  Api.addCollection(Products, getApiOptions(Products, 'Products'));
-  Api.addCollection(Orders, getApiOptions(Orders, 'Orders'));
-  Api.addCollection(Cart, getApiOptions(Cart, 'Cart'));
-  Api.addCollection(Accounts, getApiOptions(Accounts, 'Accounts'));
-  Api.addCollection(Emails, getApiOptions(Emails, 'Emails'));
-  Api.addCollection(Shipping, getApiOptions(Shipping, 'Shipping'));
+  Api.addRoute('Accounts', { authRequired: true}, {
+    get: function() {
+      const email = this.user.emails[0].address;
+      const query = hasPermission(this.user, "admin") ? {} :
+      { emails : email }
+      const allRecords = Accounts.find(query).fetch();
+        return { data: allRecords };
+    } 
+  });
+
+  Api.addCollection(Shops, getApiOptions(Shops));
+  Api.addCollection(Products, getApiOptions(Products));
+  Api.addCollection(Orders, getApiOptions(Orders));
+  Api.addCollection(Cart, getApiOptions(Cart));
+  Api.addCollection(Shipping, getApiOptions(Shipping));
 };
