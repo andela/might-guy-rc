9446a2290ba2bfef1cd9101a06b5395b7f02dbcc
andela-moseni <mercy.oseni@andela.com>
2017-05-15 16:11:46 +0100

    feature(RESTful API): add CRUD operations to collections modify API routes enable CRUD operations based on priviledges

--- server/startup/restful-endpoints.js
+++ server/startup/restful-endpoints.js
@@ -4,8 +4,8 @@ import { Roles } from "meteor/alanning:roles";
 import Reaction  from "/server/api/core";
 
 const hasPermission = (user, role) => {
-  return user.roles[Reaction.getShopId()].includes(role)
-}
+  return user.roles[Reaction.getShopId()].includes(role);
+};
 
 export default () => {
   // Global API configuration
@@ -14,7 +14,7 @@ export default () => {
     prettyJson: true,
     defaultHeaders: {
       "Content-Type": "application/json"
-    },
+    }
   });
 
   const getApiOptions = (collectionName) => {
@@ -22,70 +22,140 @@ export default () => {
       routeOptions: {
         authRequired: true
       },
-      
+
       endpoints: {
+         // GET all items in collection
+        get: {
+          action() {
+            if (hasPermission(this.user, "admin") ||
+            hasPermission(this.user, "guest") ||
+            hasPermission(this.user, "owner")) {
+              const allRecords = collectionName.findOne(this.urlParams.id);
+              return { statusCode: 200, status: "success", data: allRecords };
+            }
+            return { statusCode: 404, status: "fail", message: "record does not exist" };
+          }
+        },
+
         // POST into a collection
         post: {
-          authRequired: true,
-          roleRequired: ["admin", "owner", "guest"],
           action() {
-            const isInserted = collectionName.insert(this.bodyParams);
-            if (isInserted) {
+            if (hasPermission(this.user, "admin") ||
+            hasPermission(this.user, "guest") ||
+            hasPermission(this.user, "owner")) {
+              const isInserted = collectionName.insert(this.bodyParams);
               return { statusCode: 201, status: "success", data: isInserted };
             }
-            return { status: "fail", message: "post was not successful" };
+            return { status: "fail",
+              message: "An error occurred. Post was not successful" };
           }
         },
 
         // UPDATE a collection
         put: {
-          authRequired: true,
-          roleRequired: ["admin", "owner", "guest"],
           action() {
-            const isUpdated = collectionName.update(this.urlParams.id, {
-              $set: this.bodyParams
-            });
-            if (isUpdated) {
-              return { statusCode: 201, status: "success", data: isUpdated };
+            if (hasPermission(this.user, "admin")) {
+              const isUpdated = collectionName.update(this.urlParams.id, {
+                $set: this.bodyParams
+              });
+              return { statusCode: 200, status: "success", data: isUpdated };
             }
-            return { status: "fail", message: "record not found" };
+            return { status: "fail", message: "record does not exist" };
           }
         },
 
         // DELETE a record in a collection
         delete: {
-          authRequired: true,
-          roleRequired: ["admin"],
           action() {
-            const isDeleted = collectionName.remove(this.urlParams.id);
-            if (isDeleted) {
-              return { status: "success", data: { message: "record deleted" } };
+            if (hasPermission(this.user, "admin")) {
+              collectionName.remove(this.urlParams.id);
+              return { status: "success", data: { message: "record is deleted" } };
             }
-            return { status: "fail", message: "record not found" };
-          } 
+            return { statusCode: 404, status: "fail", message: "record does not exist" };
+          }
         }
       }
     };
   };
-  
-  Api.addRoute('Emails', { authRequired: true}, {
-    get: function() {
+
+  Api.addRoute("Emails", { authRequired: true }, {
+    get: function () {
       const email = this.user.emails[0].address;
       const query = hasPermission(this.user, "admin") ? {} :
-      { $or: [{to : email  }, {from : email }]}
+      { $or: [{ to: email  }, { from: email }] };
       const allRecords = Emails.find(query).fetch();
-        return { data: allRecords };
-    } 
+      return { statusCode: 200, status: "success", data: allRecords };
+    },
+    // POST into Emails collection
+    post: function () {
+      if (hasPermission(this.user, "admin") ||
+        hasPermission(this.user, "guest") ||
+        hasPermission(this.user, "owner")) {
+        const isInserted = collectionName.insert(this.bodyParams);
+        return { statusCode: 201, status: "success", data: isInserted };
+      }
+      return { status: "fail",
+        message: "An error occurred. Post was not successful" };
+    },
+    // UPDATE an Email collection
+    put: function () {
+      if (hasPermission(this.user, "admin")) {
+        const isUpdated = collectionName.update(this.urlParams.id, {
+          $set: this.bodyParams
+        });
+        return { statusCode: 200, status: "success", data: isUpdated };
+      }
+      return { status: "fail", message: "record does not exist" };
+    },
+    // DELETE a record in Emails collection
+    delete:
+      function () {
+        if (hasPermission(this.user, "admin")) {
+          collectionName.remove(this.urlParams.id);
+          return { status: "success", data: { message: "record is deleted" } };
+        }
+        return { statusCode: 404, status: "fail", message: "record does not exist" };
+      }
   });
 
-  Api.addRoute('Accounts', { authRequired: true}, {
-    get: function() {
-      const email = this.user.emails[0].address;
+  Api.addRoute("Accounts", { authRequired: true }, {
+    get: function () {
+      const user = this.user._id;
       const query = hasPermission(this.user, "admin") ? {} :
-      { emails : email }
+      { userId: user };
       const allRecords = Accounts.find(query).fetch();
-        return { data: allRecords };
-    } 
+      return { statusCode: 200, status: "success", data: allRecords };
+    },
+    // POST into Accounts collection
+    post: function () {
+      if (hasPermission(this.user, "admin") ||
+        hasPermission(this.user, "guest") ||
+        hasPermission(this.user, "owner")) {
+        const isInserted = collectionName.insert(this.bodyParams);
+        return { statusCode: 201, status: "success", data: isInserted };
+      }
+      return { status: "fail",
+        message: "An error occurred. Post was not successful" };
+    },
+    // UPDATE an Account collection
+    put: function () {
+      if (hasPermission(this.user, "admin")) {
+        const isUpdated = collectionName.update(this.urlParams.id, {
+          $set: this.bodyParams
+        });
+        return { statusCode: 200, status: "success", data: isUpdated };
+      }
+      return { status: "fail", message: "record does not exist" };
+    },
+    // DELETE a record in Account collection
+    delete:
+      function () {
+        if (hasPermission(this.user, "admin")) {
+          collectionName.remove(this.urlParams.id);
+          return { status: "success", data: { message: "record is deleted" } };
+        }
+        return { statusCode: 404, status: "fail", message: "record does not exist" };
+      }
   });
 
   Api.addCollection(Shops, getApiOptions(Shops));
